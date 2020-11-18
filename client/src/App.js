aeb900fb2c64e99aec5be8ef1d9f93409f9db4ec
Akshit Soneji <43265893+AKSHIT989@users.noreply.github.com>
2020-11-18 13:20:44 +0530

    Repository initiated

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Letter of Recommendation Charusat
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
