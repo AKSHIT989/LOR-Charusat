e1bd228d42c010da9e033fb277a55b0e593b304b
Akshit Soneji <43265893+AKSHIT989@users.noreply.github.com>
2021-01-20 21:06:40 +0530

    Sidebar added

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,28 @@
 import './App.css';
+import Sidebar from "./components/Sidebar/Sidebar"
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import "./assets/styles/tailwind.css"
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Letter of Recommendation Charusat
-        </p>
-      </header>
-    </div>
+    // <div className="App">
+    // <Sidebar/>
+    //   <header className="App-header">
+    //     <p>
+    //       Letter of Recommendation Charusat
+    //     </p>
+    //   </header>
+    <>
+
+    {/* <Sidebar/> */}
+    <BrowserRouter>
+    <Switch>
+      <Route path="/admin" component={Sidebar} />
+    </Switch>
+  </BrowserRouter>
+  </>
+    // </div>
   );
 }
 
