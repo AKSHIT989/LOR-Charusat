62cba8cd41f6692006af5d5088a2c53b885bd2be
Nihal Shaikh <nihalshaikh196.11@gmail.com>
2021-01-30 16:23:20 +0530

    login-Register Pages added

---
+++ client/src/views/Auth/Register/Captcha.js
@@ -0,0 +1,104 @@
+import React,{useState} from "react";
+import { Link } from "react-router-dom";
+import ReCAPTCHA from "react-google-recaptcha";
+
+function onChange(value) {
+  console.log("Captcha value:", value)
+}
+
+export default function Captcha() {
+
+  return (
+    <>
+      <div className="container mx-auto px-4 h-full">
+        <div className="flex content-center items-center justify-center h-full">
+          <div className="w-full lg:w-4/12 px-4">
+            <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-gray-300 border-0">
+              <div className="rounded-t mb-0 px-6 py-6">
+                <div className="text-center mb-3"> 
+                  <h6 className="text-gray-600 text-lg font-bold">
+                    One more step
+                  </h6>
+                </div>
+
+                <hr className="mt-6 border-b-1 border-gray-400" />
+              </div>
+              <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
+                
+                <form>
+
+                
+                  {/* <div className="relative w-full mb-3">
+                  <div className="relative w-full mb-3">
+                    <label
+                      className="block text-gray-700 text-sm font-bold mb-2"
+                      htmlFor="grid-password"
+                    >
+                      Before proceeding, we need to make sure a real person is creating this account.
+                    </label>
+                  </div>
+                    <label
+                      className="block uppercase text-gray-700 text-xs font-bold mb-2"
+                      htmlFor="grid-password"
+                    >
+                      Enter The Characters you see
+                    </label>
+                    <input
+                      type="number"
+                      className="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150"
+                      placeholder="Enter Code"
+                    />
+                  </div> */}
+                  
+                  <ReCAPTCHA sitekey="6Le0vz4aAAAAAMU7pRDBx9L-l3QGqqf-9M1zfHwu" onChange={onChange} />
+
+                  <div>
+                <label className="inline-flex items-center cursor-pointer">
+                    <input
+                        id="customCheckLogin"
+                        type="checkbox"
+                        className="form-checkbox text-gray-800 ml-1 w-5 h-5 ease-linear transition-all duration-150"
+                    />
+                    <span className="ml-2 text-sm font-semibold text-gray-700">
+                        I agree with the{" "}
+                    <a
+                      href="#pablo"
+                      className="text-blue-500"
+                      onClick={(e) => e.preventDefault()}
+                    >
+                        Privacy Policy
+                    </a>
+                    </span>
+                </label>
+              </div>
+              
+                  <div className="text-right mt-6">
+                    <button
+                      className="bg-gray-900 text-white active:bg-gray-700 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-auto  transition-all duration-150"
+                      type="button"
+                    >
+                       <Link to="/auth/AccountDetails">
+                        Back
+                    </Link>
+                    </button>
+
+                    <button
+                      className="bg-gray-900 text-white active:bg-gray-700 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ml-2 w-auto ease-linear transition-all duration-150"
+                      type="button"
+                    >
+                      <Link to="/auth/VerifyEmail">
+                        Next
+                    </Link>
+                    </button>
+
+                  </div>
+
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
