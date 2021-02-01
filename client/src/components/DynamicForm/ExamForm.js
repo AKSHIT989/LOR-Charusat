fd02a78a7eea1bc5169edb4e1392e4e6b0d565e6
Vaspar-a <vaspudie@gmail.com>
2021-02-01 22:47:55 +0530

    Removed bug from student forms

--- client/src/components/DynamicForm/ExamForm.js
+++ client/src/components/DynamicForm/ExamForm.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import SecondaryButtonOutline from "../Button/SecondaryButton"
-import InfoButton from "../Button/InfoButton"
+import React from "react";
+import SecondaryButtonOutline from "../Button/SecondaryButton";
+import InfoButton from "../Button/InfoButton";
 
 export default class ExamForm extends React.Component {
   constructor(props) {
@@ -8,89 +8,112 @@ export default class ExamForm extends React.Component {
 
     this.state = {
       message: "",
-      items: [{
-        examName: "",
-        marks:0,
-        proof:""
-      }]
-    }
+      items: [
+        {
+          examName: "",
+          marks: 0,
+          proof: "",
+        },
+      ],
+    };
+
+    this.updateMessage = this.updateMessage.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.handleExamNameChanged = this.handleExamNameChanged.bind(this);
+    this.handleMarksChanged = this.handleMarksChanged.bind(this);
+    this.handleFileChanged = this.handleFileChanged.bind(this);
+    this.handleItemDeleted = this.handleItemDeleted.bind(this);
+    this.renderRows = this.renderRows.bind(this);
   }
 
   updateMessage(event) {
+    event.stopPropagation();
     this.setState({
-      message: event.target.value
+      message: event.target.value,
     });
   }
-  
-  handleClick() {
-    var items = this.state.items;
-    
+
+  handleClick(event) {
+    event.stopPropagation();
+    let items = this.state.items;
+
     items.push({
       examName: "",
-      marks:0,
-      proof:""
+      marks: 0,
+      proof: "",
     });
-    
+
     this.setState({
       items: items,
-      message: ""
+      message: "",
     });
     // console.log(this.state.items)
   }
 
-  handleExamNameChanged(i, event) {
-    var items = this.state.items;
-    items[i].examName  = event.target.value;
+  handleExamNameChanged(event, i) {
+    event.stopPropagation();
+    let items = this.state.items;
+    items[i].examName = event.target.value;
     this.setState({
-      items: items
-    });
-    // console.log(this.state.items)
-  }
-  handleMarksChanged(i, event) {
-    var items = this.state.items;
-    items[i].marks  = event.target.value;
-    this.setState({
-      items: items
-    });
-    // console.log(this.state.items)
-  }
-  handleFileChanged(i, event) {
-    var items = this.state.items;
-    items[i].proof  = event.target.value;
-    this.setState({
-      items: items
+      items: items,
     });
     // console.log(this.state.items)
   }
 
-  handleItemDeleted(i) {
-    var items = this.state.items;
+  handleMarksChanged(event, i) {
+    event.stopPropagation();
+    let items = this.state.items;
+    items[i].marks = event.target.value;
+    this.setState({
+      items: items,
+    });
+    // console.log(this.state.items)
+  }
+
+  handleFileChanged(event, i) {
+    event.stopPropagation();
+    let items = this.state.items;
+    items[i].proof = event.target.value;
+    this.setState({
+      items: items,
+    });
+    // console.log(this.state.items)
+  }
+
+  handleItemDeleted(event, i) {
+    event.stopPropagation();
+    let items = this.state.items;
 
     items.splice(i, 1);
 
     this.setState({
-      items: items
+      items: items,
     });
   }
 
   renderRows() {
-    var context = this;
     // console.log("this", this.state)
 
     // console.log(this.state.items)
-    return  this.state.items.map(function(o, i) {
-        return (
+    return this.state.items.map((o, i) => {
+      return (
         <tr key={"item-" + i}>
-            <td>
+          <td>
             {/* <input
                 type="text"
                 value={o}
-                onChange={context.handleItemChanged.bind(context, i)}
+                onChange={this.handleItemChanged.bind(this, i)}
             /> */}
-             <select name="projectName" className="px-2 py-2 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150" 
-             onChange={context.handleExamNameChanged.bind(context, i)}
-             placeholder="Hello">
-             <option disabled selected>Select Exam</option>
+            <select
+              name="projectName"
+              className="px-2 py-2 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150"
+              onChange={(event) => this.handleExamNameChanged(event, i)}
+              placeholder="Hello"
+              // value="Selected Exam"
+            >
+              <option disabled selected>
+                Select Exam
+              </option>
               <option value="GRE">GRE</option>
               <option value="IELTS">IELTS</option>
               <option value="TOEFL">TOEFL</option>
@@ -98,68 +121,67 @@ export default class ExamForm extends React.Component {
               <option value="GATE">GATE</option>
               <option value="CAT">CAT</option>
             </select>
-            </td>
-            <td>
+          </td>
+          <td>
             <input
-                type="number"
-                className="px-2 py-2 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150"
-                placeholder="Enter marks here"
-                // value={o}
-                onChange={context.handleMarksChanged.bind(context, i)}
+              type="number"
+              className="px-2 py-2 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150"
+              placeholder="Enter marks here"
+              value={o.marks}
+              onChange={(event) => this.handleMarksChanged(event, i)}
             />
-            </td>
-            <td>
-            <input type="file" accept="application/pdf" className="px-2 py-2 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus]:shadow-outline w-full ease-linear transition-all duration-150" onChange={context.handleFileChanged.bind(context, i)}
+          </td>
+          <td>
+            <input
+              type="file"
+              accept="application/pdf"
+              className="px-2 py-2 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus]:shadow-outline w-full ease-linear transition-all duration-150"
+              onChange={(event) => this.handleFileChanged(event, i)}
             ></input>
-            </td>
-            <td>
-            <div >
-            <SecondaryButtonOutline
-                onClick={context.handleItemDeleted.bind(context, i)}
+          </td>
+          <td>
+            <div>
+              <SecondaryButtonOutline
+                onClick={(event) => this.handleItemDeleted(event, i)}
                 text="Delete"
-            >
-            </SecondaryButtonOutline>
+              ></SecondaryButtonOutline>
             </div>
-            </td>
+          </td>
         </tr>
-        );
+      );
     });
   }
 
   render() {
     return (
-        <>
-        <InfoButton
-        type="button"
-        onClick={this.handleClick.bind(this)}
-        text="Add Item"
-        />
+      <>
+        <InfoButton type="button" onClick={this.handleClick} text="Add Item" />
         {/* </InfoButton> */}
         <div className="block w-full overflow-x-auto">
-        <table className="items-center w-full bg-transparent border-collapse">
-          <thead>
-            <tr>
-              <th className="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-no-wrap font-semibold text-left bg-gray-100 text-gray-600 border-gray-200">
-                Exam Name
-              </th>
-              <th className="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-no-wrap font-semibold text-left bg-gray-100 text-gray-600 border-gray-200">
-                Marks
-              </th>
-              <th className="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-no-wrap font-semibold text-left bg-gray-100 text-gray-600 border-gray-200">
-                Upload Proof
-              </th>
-              {/* <th className="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-no-wrap font-semibold text-left bg-gray-100 text-gray-600 border-gray-200">
+          <table className="items-center w-full bg-transparent border-collapse">
+            <thead>
+              <tr>
+                <th className="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-no-wrap font-semibold text-left bg-gray-100 text-gray-600 border-gray-200">
+                  Exam Name
+                </th>
+                <th className="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-no-wrap font-semibold text-left bg-gray-100 text-gray-600 border-gray-200">
+                  Marks
+                </th>
+                <th className="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-no-wrap font-semibold text-left bg-gray-100 text-gray-600 border-gray-200">
+                  Upload Proof
+                </th>
+                {/* <th className="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-no-wrap font-semibold text-left bg-gray-100 text-gray-600 border-gray-200">
                 Actions
               </th> */}
-            </tr>
-          </thead>
-          <tbody>
-            {this.renderRows()}
-            {/* {console.log(this.state.items)} */}
-          </tbody>
-        </table>
-        <hr/>
-      </div>
+              </tr>
+            </thead>
+            <tbody>
+              {this.renderRows()}
+              {/* {console.log(this.state.items)} */}
+            </tbody>
+          </table>
+          <hr />
+        </div>
       </>
     );
   }
--- client/src/components/DynamicForm/UniversityForm.js
+++ client/src/components/DynamicForm/UniversityForm.js
@@ -1,179 +1,192 @@
-import React,{useState} from 'react';
-import SecondaryButtonOutline from "../Button/SecondaryButton"
-import InfoButton from "../Button/InfoButton"
-import DatePicker from "react-datepicker"
-import "react-datepicker/dist/react-datepicker.css"
+import React from "react";
+import SecondaryButtonOutline from "../Button/SecondaryButton";
+import InfoButton from "../Button/InfoButton";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
-export default class UniversityForm extends React.Component{
+export default class UniversityForm extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
       message: "",
-      items: [{
-        universityName: "",
-        courseName: "",
-        countryName:"",
-        intakeDate: null
-      }]
-    } 
+      items: [
+        {
+          universityName: "",
+          courseName: "",
+          countryName: "",
+          intakeDate: null,
+        },
+      ],
+    };
+    
+    this.updateMessage = this.updateMessage.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.handleUniversityNameChanged = this.handleUniversityNameChanged.bind(this);
+    this.handleCourseChanged = this.handleCourseChanged.bind(this);
+    this.handleCountryChanged = this.handleCountryChanged.bind(this);
+    this.handleItemDeleted = this.handleItemDeleted.bind(this);
+    this.renderRows = this.renderRows.bind(this);
   }
 
   updateMessage(event) {
     this.setState({
-      message: event.target.value
+      message: event.target.value,
     });
     // console.log(event.target.value)
   }
-  
-  handleClick() {
-    var items = this.state.items;
-    
+
+  handleClick(event) {
+    event.stopPropagation();
+    let items = this.state.items;
+
     items.push({
       universityName: "",
       courseName: "",
-      countryName:"",
-      intakeDate: ""
+      countryName: "",
+      intakeDate: "",
     });
-    
+
     this.setState({
       items: items,
-      message: ""
+      message: "",
     });
     // console.log(this.state.items)
   }
 
-  handleUniversityNameChanged(i, event) {
-    var items = this.state.items;
-    items[i].universityName  = event.target.value;
+  handleUniversityNameChanged(event, i) {
+    event.stopPropagation();
+    let items = this.state.items;
+    items[i].universityName = event.target.value;
     this.setState({
-      items: items
-    });
-    // console.log(this.state.items)
-  }
-  handleCourseChanged(i, event) {
-    var items = this.state.items;
-    items[i].courseName  = event.target.value;
-    this.setState({
-      items: items
-    });
-    // console.log(this.state.items)
-  }
-  handleCountryChanged(i, event) {
-    var items = this.state.items;
-    items[i].countryName  = event.target.value;
-    this.setState({
-      items: items
+      items: items,
     });
     // console.log(this.state.items)
   }
 
-  handleItemDeleted(i) {
-    var items = this.state.items;
+  handleCourseChanged(event, i) {
+    event.stopPropagation();
+    let items = this.state.items;
+    items[i].courseName = event.target.value;
+    this.setState({
+      items: items,
+    });
+    // console.log(this.state.items)
+  }
+
+  handleCountryChanged(event, i) {
+    event.stopPropagation();
+    let items = this.state.items;
+    items[i].countryName = event.target.value;
+    this.setState({
+      items: items,
+    });
+    // console.log(this.state.items)
+  }
+
+  handleItemDeleted(event, i) {
+    event.stopPropagation();
+    let items = this.state.items;
 
     items.splice(i, 1);
 
     this.setState({
-      items: items
+      items: items,
     });
   }
 
-
   renderRows() {
-    var context = this;
-    // console.log("this", this.state)
-
-    // console.log(this.state.items)
-    return  this.state.items.map(function(o, i) {
-        return (
+    return this.state.items.map((o, i) => {
+      return (
         <tr key={"item-" + i}>
-            <td>
-             <input name="projectName" className="px-2 py-2 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150" 
-             onChange={context.handleUniversityNameChanged.bind(context, i)}
-             placeholder="University Name here">
-            </input>
-            </td>
-            <td>
+          <td>
             <input
-                type="text"
-                className="px-2 py-2 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150"
-                placeholder="Course Name here"
-                // value={o}
-                onChange={context.handleCourseChanged.bind(context, i)}
-            />
-            </td>
-            <td>
+              name="projectName"
+              className="px-2 py-2 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150"
+              onChange={(event) => this.handleUniversityNameChanged(event, i)}
+              placeholder="University Name here"
+              value={o.universityName}
+            ></input>
+          </td>
+          <td>
             <input
-                type="text"
-                className="px-2 py-2 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150"
-                placeholder="Country Name here"
-                // value={o}
-                onChange={context.handleCountryChanged.bind(context, i)}
+              type="text"
+              className="px-2 py-2 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150"
+              placeholder="Course Name here"
+              value={o.courseName}
+              onChange={(event) => this.handleCourseChanged(event, i)}
             />
-            </td>
-            <td>
+          </td>
+          <td>
+            <input
+              type="text"
+              className="px-2 py-2 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150"
+              placeholder="Country Name here"
+              value={o.countryName}
+              onChange={(event) => this.handleCountryChanged(event, i)}
+            />
+          </td>
+          <td>
             <div className="px-2 py-2 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150">
-            <DatePicker 
-            placeholderText="Select date here"
-              dateFormat="MMM/yyyy"
-              showMonthYearPicker
-              selected={context.state.items[i].intakeDate}
-              onChange={ date=>{
-                var items = context.state.items;
-                items[i].intakeDate = date;
-                context.setState({
-                  items: items
-                });
-              } }
+              <DatePicker
+                placeholderText="Select date here"
+                dateFormat="MMM/yyyy"
+                showMonthYearPicker
+                selected={o.intakeDate}
+                onChange={(date, event) => {
+                  event.stopPropagation();
+                  let items = this.state.items;
+                  items[i].intakeDate = date;
+                  this.setState({
+                    items: items,
+                  });
+                }}
               />
             </div>
-            </td>
-            <td>
-            <div >
-            <SecondaryButtonOutline
-                onClick={context.handleItemDeleted.bind(context, i)}
+          </td>
+          <td>
+            <div>
+              <SecondaryButtonOutline
+                onClick={(event) => this.handleItemDeleted(event, i)}
                 text="Delete"
-            >
-            </SecondaryButtonOutline>
+              ></SecondaryButtonOutline>
             </div>
-            </td>
+          </td>
         </tr>
-        );
+      );
     });
   }
 
   render() {
     return (
-        <>
+      <>
         <InfoButton
-        type="button"
-        onClick={this.handleClick.bind(this)}
-        text="Add Item"
+          type="button"
+          onClick={this.handleClick}
+          text="Add Item"
         />
         <div className="block w-full overflow-x-auto">
-        <table className="items-center w-full bg-transparent border-collapse">
-          <thead>
-            <tr>
-              <th className="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-no-wrap font-semibold text-left bg-gray-100 text-gray-600 border-gray-200">
-                 University Name
-               </th>
-               <th className="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-no-wrap font-semibold text-left bg-gray-100 text-gray-600 border-gray-200">
-                 Course Name
-               </th>
-               <th className="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-no-wrap font-semibold text-left bg-gray-100 text-gray-600 border-gray-200">
-                 Country Name
-               </th>
-               <th className="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-no-wrap font-semibold text-left bg-gray-100 text-gray-600 border-gray-200">
-                 Intake(Month-Year)
-               </th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.renderRows()}
-          </tbody>
-        </table>
-        <hr/>
-      </div>
+          <table className="items-center w-full bg-transparent border-collapse">
+            <thead>
+              <tr>
+                <th className="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-no-wrap font-semibold text-left bg-gray-100 text-gray-600 border-gray-200">
+                  University Name
+                </th>
+                <th className="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-no-wrap font-semibold text-left bg-gray-100 text-gray-600 border-gray-200">
+                  Course Name
+                </th>
+                <th className="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-no-wrap font-semibold text-left bg-gray-100 text-gray-600 border-gray-200">
+                  Country Name
+                </th>
+                <th className="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-no-wrap font-semibold text-left bg-gray-100 text-gray-600 border-gray-200">
+                  Intake(Month-Year)
+                </th>
+              </tr>
+            </thead>
+            <tbody>{this.renderRows()}</tbody>
+          </table>
+          <hr />
+        </div>
       </>
     );
   }
@@ -183,16 +196,16 @@ export default class UniversityForm extends React.Component{
 
 //   const [message, setMessage] = useState("")
 //   const [items, setItems] = useState([{ }])
-  
+
 //   const updateMessage = (event)=> {
 //     setMessage(event.target.value);
 //     console.log(event.target.value)
 //     // return message
 //   }
-  
+
 //   const handleClick = () => {
 //     var item = items;
-    
+
 //     item.push({ });
 //     setMessage("");
 //     setItems(item);
@@ -216,13 +229,13 @@ export default class UniversityForm extends React.Component{
 
 //   function renderRows() {
 //     // console.log("this", this)
-    
+
 //     // console.log(items)
 //     return  items.map(function(o, i) {
 //         return (
 //         <tr key={"item-" + i}>
 //             <td>
-//              <input name="UniversityName" className="px-2 py-2 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150" 
+//              <input name="UniversityName" className="px-2 py-2 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150"
 //              onChange={(event)=>{
 //                updateMessage(event)
 //                handleItemChanged(i,event)
@@ -258,7 +271,7 @@ export default class UniversityForm extends React.Component{
 //             {/* <input
 //                 type="text"
 //                 value={o}
-//                 onChange={context.handleItemChanged.bind(context, i)}
+//                 onChange={this.handleItemChanged.bind(this, i)}
 //             /> */}
 //             </td>
 //             <td>
