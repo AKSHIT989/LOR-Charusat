366bcf93a8fee8c61e0215400f9c6812f46ed70e
Akshit Soneji <43265893+AKSHIT989@users.noreply.github.com>
2021-06-22 09:26:01 +0530

    contributing.md added

---
+++ CONTRIBUTING.md
@@ -0,0 +1,39 @@
+# LOR-Charusat
+Automating the process of Letter of Recommendation for CHARUSAT
+
+### Process for contributing for collaborators:
+NOTE: DO NOT CHANGE THE FOLDER STRUCTURE IT WON'T BE MERGED
+1) For first time only 
+```
+git clone https://github.com/AKSHIT989/LOR-Charusat.git
+cd LOR-Charusat/
+```
+2) Check the task list in project section https://github.com/AKSHIT989/LOR-Charusat/projects
+
+3) For performing task you are supposed to create a branch with name associated and move the task from "To do" to "In progress".
+Example: For Frontend project, To perform ``` 1:Build common components``` branch name should ```Frontend#1``` and move the card #1 from "To do" to "In progress"
+
+You can checkout to a different branch using this command: 
+```
+git checkout -b <branch-name>
+```
+
+4) For working on Frontend/Backend
+```
+cd client/server
+npm install
+``` 
+5) After completing your work you are supposed to push the changes back to remote
+```
+git add .
+git commit -m "proper-message"
+git push origin <branch-name>
+```
+Now move the task in project section from "In progress" to "Done"
+
+6) If all things are good make a pull request but ```DON'T``` merge it directly.
+
+7) If you are supposed to work on a different task then synchronise your local copy with remote repo and then perform task
+```
+git pull origin main
+```
--- client/src/components/Navbar/AuthNavbar.js
+++ client/src/components/Navbar/AuthNavbar.js
@@ -4,32 +4,29 @@ import { Link } from "react-router-dom";
 
 // components
 
-
-
 export default function Navbar(props) {
-  const [navbarOpen, setNavbarOpen] = React.useState(false);
-  return (
-    <>
-      <nav className="top-0 absolute z-50 w-full flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg">
-        <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
-          <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
-            <Link
-              className="text-blue-600 text-xl font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase"
-              to="/"
-            >
-              Charusat-LOR
-            </Link>
-            <button
-              className="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
-              type="button"
-              onClick={() => setNavbarOpen(!navbarOpen)}
-            >
-              <i className="text-white fas fa-bars"></i>
-            </button>
-          </div>
-       
-        </div>
-      </nav>
-    </>
-  );
+	const [navbarOpen, setNavbarOpen] = React.useState(false);
+	return (
+		<>
+			<nav className="top-0 absolute z-50 w-full flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg">
+				<div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+					<div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+						<Link
+							className="text-blue-600 text-xl font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase"
+							to="/"
+						>
+							LOR-CHARUSAT
+						</Link>
+						<button
+							className="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+							type="button"
+							onClick={() => setNavbarOpen(!navbarOpen)}
+						>
+							<i className="text-white fas fa-bars"></i>
+						</button>
+					</div>
+				</div>
+			</nav>
+		</>
+	);
 }
