178b19bcac6dd8bc612fa01797aff383f7c5fef2
Vaspar-a <vaspudie@gmail.com>
2021-07-23 17:04:29 +0530

    Merged Local Test

--- strapi/my-project/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '82f1dda83b7e6377016ee3ca1831251b'),
-    },
-  },
-});
--- strapi/my-project/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '55b10fc1-bcee-49ff-be18-9d76c3c40f12'
-};
