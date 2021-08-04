548154eda88db2f3c4d30bcf825f3037267aec71
Nihal Shaikh <nihalshaikh196.11@gmail.com>
2021-08-04 22:43:47 +0530

    #12 fixed

--- client/src/views/Auth/Register/PersonalDetails.js
+++ client/src/views/Auth/Register/PersonalDetails.js
@@ -9,7 +9,7 @@ export default function PersonalDetails() {
           <div className="w-full lg:w-4/12 px-4">
             <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-gray-300 border-0">
               <div className="rounded-t mb-0 px-6 py-6">
-                <div className="text-center mb-3"> 
+                <div className="text-center mb-3">
                   <h6 className="text-gray-600 text-lg font-bold">
                     Personal Details
                   </h6>
@@ -18,14 +18,13 @@ export default function PersonalDetails() {
                 <hr className="mt-6 border-b-1 border-gray-400" />
               </div>
               <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
-                
                 <form>
                   <div className="relative w-full mb-3">
                     <label
                       className="block uppercase text-gray-700 text-xs font-bold mb-2"
                       htmlFor="grid-password"
                     >
-                      Name
+                      First Name
                     </label>
                     <input
                       type="text"
@@ -39,12 +38,12 @@ export default function PersonalDetails() {
                       className="block uppercase text-gray-700 text-xs font-bold mb-2"
                       htmlFor="grid-password"
                     >
-                      Department
+                      Last Name
                     </label>
                     <input
                       type="text"
                       className="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150"
-                      placeholder="Department"
+                      placeholder="Name"
                     />
                   </div>
 
@@ -67,6 +66,20 @@ export default function PersonalDetails() {
                       className="block uppercase text-gray-700 text-xs font-bold mb-2"
                       htmlFor="grid-password"
                     >
+                      Department
+                    </label>
+                    <input
+                      type="text"
+                      className="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150"
+                      placeholder="Department"
+                    />
+                  </div>
+
+                  {/* <div className="relative w-full mb-3">
+                    <label
+                      className="block uppercase text-gray-700 text-xs font-bold mb-2"
+                      htmlFor="grid-password"
+                    >
                       Counsellor
                     </label>
                     <input
@@ -74,27 +87,26 @@ export default function PersonalDetails() {
                       className="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150"
                       placeholder="Counsellor"
                     />
-                  </div>
+                  </div> */}
 
                   <div className="text-right mt-6">
-                    <button
-                      className="bg-gray-900 text-white active:bg-gray-700 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-auto  transition-all duration-150"
-                      type="button"
-                    >
-                       <Link to="/auth/Register">
+                    <Link to="/auth/Register">
+                      <button
+                        className="bg-gray-900 text-white active:bg-gray-700 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-auto  transition-all duration-150"
+                        type="button"
+                      >
                         Back
+                      </button>
                     </Link>
-                    </button>
 
-                    <button
-                      className="bg-gray-900 text-white active:bg-gray-700 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ml-2 w-auto ease-linear transition-all duration-150"
-                      type="button"
-                    >
-                      <Link to="/auth/AccountDetails">
+                    <Link to="/auth/AccountDetails">
+                      <button
+                        className="bg-gray-900 text-white active:bg-gray-700 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ml-2 w-auto ease-linear transition-all duration-150"
+                        type="button"
+                      >
                         Next
+                      </button>
                     </Link>
-                    </button>
-
                   </div>
                 </form>
               </div>
--- client/src/views/Auth/Register/Register.js
+++ client/src/views/Auth/Register/Register.js
@@ -9,7 +9,7 @@ export default function Register() {
           <div className="w-full lg:w-4/12 px-4">
             <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-gray-300 border-0">
               <div className="rounded-t mb-0 px-6 py-6">
-                <div className="text-center mb-3"> 
+                <div className="text-center mb-3">
                   <h6 className="text-gray-600 text-lg font-bold">
                     Create account
                   </h6>
@@ -18,22 +18,21 @@ export default function Register() {
                 <hr className="mt-6 border-b-1 border-gray-400" />
               </div>
               <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
-                
                 <form>
-
-                <div className="relative w-full mb-3">
+                  <div className="relative w-full mb-3">
                     <input
                       type="radio"
+                      name="user"
                       className="px-3 py-3 placeholder-gray-400 text-gray-700 text-sm shadow focus:outline-none focus:shadow-outline w-5 ease-linear transition-all duration-150"
-                    /> 
-                    Student  
-
+                    />
+                    Student
                     <input
                       type="radio"
-                      style={{marginLeft:"15%"}}
+                      name="user"
+                      style={{ marginLeft: "15%" }}
                       className="px-3 py-3 placeholder-gray-400 text-gray-700 text-sm shadow focus:outline-none focus:shadow-outline w-5 ease-linear transition-all duration-150"
-                    /> 
-                    Faculty 
+                    />
+                    Faculty
                   </div>
 
                   <div className="relative w-full mb-3">
@@ -51,32 +50,29 @@ export default function Register() {
                   </div>
 
                   <div className="text-right mt-6">
-                    <button
-                      className="bg-gray-900 text-white active:bg-gray-700 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-auto ease-linear transition-all duration-150"
-                      type="button"
-                    >
-                     <Link to="/auth/PersonalDetails">
-                      Next
-                      </Link>
-                    </button>
+                    <Link to="/auth/PersonalDetails">
+                      <button
+                        className="bg-gray-900 text-white active:bg-gray-700 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-auto ease-linear transition-all duration-150"
+                        type="button"
+                      >
+                        Next
+                      </button>
+                    </Link>
                   </div>
                 </form>
               </div>
             </div>
             <div className="flex flex-wrap mt-6 relative">
               <div className="w-full">
-              Already member?
-              <Link to="/auth/login" className="text-blue-600">
+                Already member?
+                <Link to="/auth/login" className="text-blue-600">
                   Login Here
                 </Link>
               </div>
             </div>
-
-
           </div>
         </div>
       </div>
     </>
   );
 }
-
